3890ca9979b8202d8599c32b35c083d3d7395fed
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-05-09 22:10:13 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const instanceUrl = 'https://linuxrocks.online';
+
+// Fetch the public timeline from the Mastodon API
+fetch(`${instanceUrl}/api/v1/timelines/public`)
+  .then(response => response.json())
+  .then(posts => {
+    // Create an array of HTML elements representing the posts
+    const postElements = posts.map(post => {
+      return `
+        <div>
+          <p><strong>${post.account.display_name}</strong> (@${post.account.username})</p>
+          <p>${post.content}</p>
+        </div>
+      `;
+    });
+
+    // Insert the post elements into the DOM
+    const rootElement = document.getElementById('root');
+    rootElement.innerHTML = postElements.join('');
+  })
+  .catch(error => {
+    console.error(error);
+  });
